c7553ed267d916dfbe4d416358f7a1401303a9eb
Ashton B <ashtonbrugh@gmail.com>
2022-02-25 10:21:46 -0500

    completing the about me section

--- src/components/About.js/index.js
+++ src/components/About.js/index.js
@@ -4,12 +4,19 @@ function About () {
     return (
         <div className="container-about">
                <h1 className="about-header">About Me</h1>
+            <div className="about">
             <img 
-            alt="portrait of the writer" 
+            alt="the developer at the grand canyon" 
             src={portrait} className="portrait"
-            style={{ width: "40%" }}
+            style={{ width: "50%" }}
             ></img>
+            <p className="about-text">Hi! My name is Ashton Brugh and this is my portfolio using React. 
+            I am currently enrolled in the UNC Chapel Hill Coding and Web Programming Boot Camp where I discovered
+            my passion for front end development. On a personal note, I live in Raleigh, North Carolina. I enjoy
+            live music, spending time outdoors, travelling, reading, and spending time with my family.</p>
         </div>
+        </div>
+       
     )
 }
 
