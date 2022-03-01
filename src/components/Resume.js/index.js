0dc0c017555362573ed37b6efd13acde3978d5d1
Ashton B <ashtonbrugh@gmail.com>
2022-03-01 16:41:38 -0500

    finishing resume and making the pdf downloadable

--- src/components/Resume.js/index.js
+++ src/components/Resume.js/index.js
@@ -1,6 +1,25 @@
 function Resume() {
     return(
-        <h1>resume will go here</h1>
+        <div className="resume-container">
+            <h1 className="resume-header">Resume</h1>
+            <div className="resume-list">
+                <ul className="front-end-list"> <u className="underline">Front end</u>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                    <li>React</li>
+                    <li>Python</li>
+                </ul>
+                <ul className="front-end-list"> <u className="underline">Back end</u>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>SQL</li>
+                    <li>PWAs</li>
+                    <li>NoSQL</li>
+                </ul>
+            </div>
+            <a className="resume-link" href="resume/Resume.pdf" download="Ashton Brugh Resume">Click to download my resume</a>
+        </div>
     )
 }
 
